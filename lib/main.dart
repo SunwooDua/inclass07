afee46a4b5819373d31e608cee7be76f7139e296
Sunwoo Yeom <o55306605@gmail.com>
2025-02-26 14:28:46 -0500

    added pageview and second screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,75 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class swipeScreen extends StatefulWidget {
+  const swipeScreen({super.key});
+
+  @override
+  State<swipeScreen> createState() => _swipeScreenState();
+}
+
+class _swipeScreenState extends State<swipeScreen> {
+  final controller = PageController(initialPage: 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: controller,
+        children: [FadingTextAnimation(), SecondFadingAnimation()],
+      ),
+    );
+  }
+}
+
 class FadingTextAnimation extends StatefulWidget {
   @override
   _FadingTextAnimationState createState() => _FadingTextAnimationState();
 }
 
-final controller = PageController(initialPage: 1);
-
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
-  final pageView = PageView(controller: controller, children: []);
+  bool _isVisible = true;
+  void toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Fading Text Animation')),
+      body: Center(
+        child: GestureDetector(
+          //clicking text changes opacity too
+          onTap: toggleVisibility,
+          child: AnimatedOpacity(
+            opacity: _isVisible ? 1.0 : 0.0,
+            duration: Duration(seconds: 1), //changed duration
+            curve: Curves.easeInOut, //added curve
+            child: const Text(
+              'This will disappear!', //changed text
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggleVisibility,
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
+
+class SecondFadingAnimation extends StatefulWidget {
+  const SecondFadingAnimation({super.key});
+
+  @override
+  State<SecondFadingAnimation> createState() => _SecondFadingAnimationState();
+}
+
+class _SecondFadingAnimationState extends State<SecondFadingAnimation> {
   bool _isVisible = true;
   void toggleVisibility() {
     setState(() {
@@ -40,7 +100,7 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
             duration: Duration(seconds: 2), //changed duration
             curve: Curves.easeInOut, //added curve
             child: const Text(
-              'This will disappear!', //changed text
+              'Will I disappear too?', //changed text
               style: TextStyle(fontSize: 24),
             ),
           ),
