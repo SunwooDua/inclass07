32b711f6eeef11025a556485bea080f146f2ad16
Sunwoo Yeom <o55306605@gmail.com>
2025-02-26 14:31:31 -0500

    minor fix to enable swipe

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: FadingTextAnimation());
+    return MaterialApp(home: swipeScreen());
   }
 }
 
